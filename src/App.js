6ef1b90f2976a304811f74959226ebdace003abe
iforyourajiv <sddrajiv@gmail.com>
2020-06-13 21:43:30 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navbar from './Components/layout/Navbar';
+import Users from './Components/users/Users';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
