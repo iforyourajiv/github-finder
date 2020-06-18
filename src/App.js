e0e76752504d7f2be13cf118c160b912f968d8d8
iforyourajiv <sddrajiv@gmail.com>
2020-06-18 23:23:52 +0530

    with Search Bar fetch users via  Github API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Components/layout/Navbar';
 import Users from './Components/users/Users';
+import Search from './Components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -10,17 +11,22 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
-    this.setState({ users: res.data, loading: false });
-  }
+  // async componentDidMount() {}
 
+  //Search Github Users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
+          <Search searchUsers={this.searchUsers}></Search>
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
